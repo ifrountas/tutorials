5ecef3196f25a636ce71d7e156c265c36ba0fa6e
Iakovos Frountas <iakovosfrountas@gmail.com>
2018-02-16 11:57:31 +0200

    Create htaccess-code-for-referral-links.php

---
+++ htaccess-code-for-referral-links.php
@@ -0,0 +1,6 @@
+## STOP REFERRER SPAM ##
+## SITE REFERRER BANNING
+RewriteCond %{HTTP_REFERER} floating-share-buttons.com [NC,OR]
+RewriteCond %{HTTP_REFERER} floating-share-buttons.com
+RewriteRule .* – [F]
+## STOP REFERRER SPAM ##
